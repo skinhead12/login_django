440c02f3c1af50a61ee14a7ef9ee859442ddcf7c
skinhead12 <edithr_isc@hotmail.com>
2022-04-27 15:35:09 -0500

    :white_check_mark: signup function working

--- authentication/views.py
+++ authentication/views.py
@@ -1,11 +1,32 @@
 
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 def home(request):
     return render (request, 'authentication/index.html')
 
 def signup(request):
+
+    if request.method == "POST":
+        username = request.POST ['username']
+        fname = request.POST ['fname']
+        lname = request.POST ['lname']
+        email = request.POST ['email']
+        pass1 = request.POST ['pass1']
+        pass2 = request.POST ['pass2']
+
+        myuser = User.objects.create_user (username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+
+        myuser.save()
+
+        messages.success(request, 'Your account has been succesfully created.')
+
+        return redirect('signin')
+
     return render (request, 'authentication/signup.html')
 
 def signin(request):
